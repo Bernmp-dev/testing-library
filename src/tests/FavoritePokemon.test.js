84bb09f5d4c15a3824a018d2d3e1a2e0f6b461f2
Bernardo Marques <bernardomp.dev@gmail.com>
2022-11-22 18:15:27 -0300

    3rd requirement completed

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+test('Teste o componente <FavoritePokemon.js />', () => {
+  renderWithRouter(<FavoritePokemon />);
+
+  const favTitle = screen.getByText(/no favorite pokémon found/i);
+  expect(favTitle).toBeInTheDocument();
+});
