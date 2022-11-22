5cfdc32a29bf15572c80134444f0a620804f56cc
Bernardo Marques <bernardomp.dev@gmail.com>
2022-11-22 17:51:26 -0300

    2nd requirement completed

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../pages';
+
+test('Teste o componente <About.js />.', () => {
+  renderWithRouter(<About />);
+
+  const aboutH2 = screen
+    .getByRole('heading', { name: /about pokédex/i });
+
+  const imgPokedex = screen
+    .getByRole('img', { name: /pokédex/i });
+  const imgURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+  expect(aboutH2).toBeInTheDocument();
+  expect(imgPokedex.src).toBe(imgURL);
+});
