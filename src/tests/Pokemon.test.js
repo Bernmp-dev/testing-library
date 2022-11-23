001feb0e5057b3493ae9e8187af5c32929d5479d
Bernardo Marques <bernardomp.dev@gmail.com>
2022-11-22 22:10:25 -0300

    6th requirement completed

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import pokemonList from '../data';
+
+test('Teste o componente <Pokedex.js />', () => {
+  const { history } = renderWithRouter(<App />);
+
+  // const detailsButton = screen
+  //   .getByRole('link', { name: /more details/i });
+  // const detailButton = detailsButton.href;
+
+  const pokeName = screen.getByTestId('pokemon-name').innerHTML;
+  const pokeImg = screen.getByRole('img');
+  const poketest = pokemonList.find((poke) => poke.name === pokeName);
+  const detailButton = screen.getByRole('link', { name: /more details/i });
+  const typeId = screen.getByTestId('pokemon-type');
+
+  expect(pokeImg.src).toBe(poketest.image);
+  expect(pokeImg.alt).toBe(`${pokeName} sprite`);
+
+  userEvent.click(detailButton);
+
+  const favCheck = screen
+    .getByRole('checkbox', { name: /pokémon favoritado\?/i });
+
+  userEvent.click(favCheck);
+
+  const favImg = screen
+    .getByRole('img', { name: `${pokeName} is marked as favorite` });
+
+  expect(favImg.src).toBe('http://localhost/star-icon.svg');
+  expect(favImg.alt).toBe(`${pokeName} is marked as favorite`);
+  expect(typeId).toHaveTextContent(poketest.type);
+  expect(detailButton.href).toBe(`http://localhost/pokemon/${poketest.id}`);
+});
